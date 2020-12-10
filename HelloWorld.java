bb99f994e112c3a48d65942bbbd554511050745a
amit kulkarni <pythoncpp@gmail.com>
2020-12-10 07:44:56 +0000

    removed all the test lines;

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,17 +1,6 @@
 public class HelloWorld {
   public static void main(String args[]) {
     System.out.println("Hello World from app");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
-    System.out.println("added another line");
     int num = 100;
   }
 }
